ae3e8ed45d3c1c06c280040faee5b2afb2c60bfa
2Barbosa <fedorovba3@gmail.com>
2023-07-26 22:33:03 +0900

    KS-1-2

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,10 @@
+package lesson_1
+
+fun main() {
+    var numberOrders: Byte  = 75
+    var notificationText: String = "Благодарим за покупку!"
+    var summWorkers = 2000
+ // println(summWorkers)
+    summWorkers = 2000 - 1
+    println(summWorkers)
+}
