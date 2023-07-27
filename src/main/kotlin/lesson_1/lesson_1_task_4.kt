ffc3444c31fa88c3bc1e92af1ed3773b54bcaf12
2Barbosa <fedorovba3@gmail.com>
2023-07-27 22:09:07 +0900

    KS-1-4

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,16 @@
+package lesson_1
+
+fun main() {
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+    println("Расстояние в миллиметрах - $length")
+    println("Возраст Гагарина на маомент полета - $age")
+    println("Часть дня - $partOfDay")
+    println("Секунды - $seconds")
+    println("Часть года - $partOfYear")
+    println("Апогей орбиты - $apogee")
+}
