26495a3dba9077fd7583a2278c1aa71d222934a8
2Barbosa <fedorovba3@gmail.com>
2023-07-28 16:56:44 +0900

    KS-2-1

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main() {
+   var mark = 3.0
+   var mark1 = 4.0
+   var mark2 = 3.0
+   var mark3 = 5.0
+   println("Средний балл - ${(mark + mark1 + mark2 + mark3) / 4}")
+
+}
