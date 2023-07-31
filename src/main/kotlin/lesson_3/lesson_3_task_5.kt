a9e153e4fe34f0497c3fa92a77de5ddec40d6fbb
2Barbosa <fedorovba3@gmail.com>
2023-07-31 16:30:52 +0900

    KS-3-5

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -2,8 +2,9 @@ package lesson_3
 
 fun main() {
     println("D2-D4;0")
-    var cageFrom = "D2"
-    var cageNext = "D4"
+    var letter = "D"
+    var number = 2
+    var number1 = 4
     var stepNum = 0
-    println("$cageFrom-$cageNext;$stepNum")
+    println("$letter$number-$letter$number1;$stepNum")
 }
