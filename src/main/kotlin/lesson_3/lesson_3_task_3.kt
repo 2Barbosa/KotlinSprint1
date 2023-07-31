0ccbc5746bc7ea15fb3d432645c7bc8b2e76a691
2Barbosa <fedorovba3@gmail.com>
2023-07-31 15:52:40 +0900

    KS-3-3

--- src/main/kotlin/lesson_3/lesson_3_task_3.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -3,15 +3,15 @@ package lesson_3
 fun main() {
     var num = 2
     var multiString = """
-        |${num * 1}
-        |${num * 2}
-        |${num * 3}
-        |${num * 4}
-        |${num * 5}
-        |${num * 6}
-        |${num * 7}
-        |${num * 8}
-        |${num * 9}
+        |$num x 1 = ${num * 1}
+        |$num x 2 = ${num * 2}
+        |$num x 3 = ${num * 3}
+        |$num x 4 = ${num * 4}
+        |$num x 5 = ${num * 5}
+        |$num x 6 = ${num * 6}
+        |$num x 7 = ${num * 7}
+        |$num x 8 = ${num * 8}
+        |$num x 9 = ${num * 9}
     """.trimMargin()
     println(multiString)
 }
