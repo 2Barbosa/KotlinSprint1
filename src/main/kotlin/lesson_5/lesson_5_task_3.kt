24a1e019ab5ff635503eb040c1e32d19b8d9c4eb
2Barbosa <fedorovba3@gmail.com>
2023-09-17 21:51:40 +0900

    KS-5-3

---
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -0,0 +1,21 @@
+package lesson_5
+
+fun main() {
+
+    println("Надо угадать два числа от 1 до 100")
+
+    val userNumber = readLine()!!.toInt()
+    val userNumber1 = readLine()!!.toInt()
+
+    val resultText = if (userNumber == RIGHT_NUMBER || userNumber == RIGHT_NUMBER_1) {
+        println("Поздравляем! Вы выиграли утешительный приз!")
+    } else if (userNumber == RIGHT_NUMBER && userNumber == RIGHT_NUMBER_1) {
+        println("Поздравляем! Вы выиграли утешительный приз")
+    } else {
+        println("Неудача!Крутите барабан")
+    }
+        println(resultText)
+}
+
+const val RIGHT_NUMBER = 23
+const val RIGHT_NUMBER_1 = 79
