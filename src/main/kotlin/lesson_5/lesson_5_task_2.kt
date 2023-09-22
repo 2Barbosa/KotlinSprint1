5076aa0597c3e865b9f771af581bb30e385d8b4a
2Barbosa <fedorovba3@gmail.com>
2023-09-22 20:48:25 +0900

    KS-5-2 corrected 2

--- src/main/kotlin/lesson_5/lesson_5_task_2.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -2,7 +2,9 @@ package lesson_5
 
 fun main() {
 
-    val userBirthYear = readln()!!.toInt()
+    println("Напишите год рождения")
+
+    val userBirthYear = readln().toInt()
 
     val resultText = if (userBirthYear <= BIRTH_YEAR) "Показать экран со скрытым контентом"
     else "Вернуться на главный экран"
