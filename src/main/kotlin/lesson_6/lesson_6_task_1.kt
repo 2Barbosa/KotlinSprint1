f9d69d0fe243e0b57509270fde53375abe7dcb80
2Barbosa <fedorovba3@gmail.com>
2023-09-26 16:27:06 +0900

    KS-6-1(correct)

--- src/main/kotlin/lesson_6/lesson_6_task_1.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_1.kt
@@ -1,30 +1,33 @@
 package lesson_6
 
+
 fun main() {
 
     println("Создайте имя пользователя:")
 
     val userName = readln()
-
     println("Создайте пароль:")
     val userPassword = readln()
-
-    println("Для авторицации напишите имя пользователя")
-    val resultText = if (userName == readln()) "Верно"
-    else "имя не верно"
-    println(resultText)
-
-    println("Введите пароль")
-
-    while (userPassword !== readln()) {
-
-    val resultText1 = if (userPassword == readln()) "Авторизация прошла успешно"
-
-    else "Логин неверный. Напишите еще раз"
-
-        println(resultText1)
-
-    }
+    println("Для авторизации напишите имя пользователя")
+    do {
+        val resultText = if (userName == readln()) "Имя верно,введите пароль"
+        else "имя не верно, попробуйте еще раз"
+        println(resultText)
+    } while (userName !== readln())
+    do {
+        val resultText1 = if (userPassword == readln()) "Авторизация прошла успешно"
+        else "Логин неверный. Напишите еще раз"
+        while (userPassword !== readln())
+            println(resultText1)
+    } while (userPassword !== readln())
 }
+
+
+
+
+
+
+
+
 
 
